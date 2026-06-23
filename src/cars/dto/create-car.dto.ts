35a34b22dc9de54bcada11723c7afbe0533bf404
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2026-06-23 17:20:30 -0500

    commit n

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -3,12 +3,12 @@ import { IsOptional, IsString, IsUUID, Length } from 'class-validator';
 export class CreateCarDTO {
   @IsString({ message: 'Brand Property Error: Please type brand' })
   @Length(3)
-  private readonly brand = '';
+  private readonly brand;
 
   @IsString({ message: 'Model Property Error: Please type model' })
   @Length(3)
-  private readonly model = '';
+  private readonly model;
 
   @IsUUID('4')
-  private readonly uuid = '';
+  private readonly uuid;
 }
