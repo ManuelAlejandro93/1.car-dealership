511bc3ebca4b00da1200518c04393df0bad057f6
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2026-06-30 15:07:33 -0500

    commit n

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars';
+import { BrandsModule } from './brands/brands.module';
 
 @Module({
   controllers: [],
   exports: [],
-  imports: [CarsModule],
+  imports: [CarsModule, BrandsModule],
   providers: [],
 })
 export class AppModule {}
---
+++ src/brands/index.ts
@@ -0,0 +1,5 @@
+export { CreateBrandDto, UpdateBrandDto } from './dto';
+export { Brand } from './entities';
+export { BrandsModule } from './brands.module';
+export { BrandsController } from './brands.controller';
+export { BrandsService } from './brands.service';
